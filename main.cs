d57720417d945146e27e159289f7eb2d49621e24
junhobaek <115634052+baek-rokaf@users.noreply.github.com>
2022-11-13 11:40:22 +0000

    30

--- main.cs
+++ main.cs
@@ -4,9 +4,13 @@ namespace HelloWorld
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        float num1; // 변수의 선언
+        num1 = 126.23f; // 변수의 대입
+        float num2 = 20.56f; // 변수의 선언과 대입
+        const float pi = 3.14f; // const 상수는 항상 선언과 대입이 동시에 이루어진다.
+
+
+
+
     }
 }
